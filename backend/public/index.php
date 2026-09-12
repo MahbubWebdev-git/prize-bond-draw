822ecb7148a4058451043348a75bb785e33acd89
MahbubWebdev-git <bdswapan@gmail.com>
2026-09-12 12:33:58 -0400

    htacess and index updated

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,20 +1,22 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+
+// LiteSpeed Subfolder Routing fix: Request URI থেকে extra path ট্রিম করা
+if (isset($_SERVER['REQUEST_URI'])) {
+    $_SERVER['REQUEST_URI'] = preg_replace('#^/prizebond_draw/backend#', '', $_SERVER['REQUEST_URI']);
+}
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// Register the Auto Loader...
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
